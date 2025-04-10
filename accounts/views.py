5e3e2a143082a5f10b05856593f606cfe1b4a0e3
VslVictor7 <vic.collfac358@outlook.com>
2025-04-09 22:31:54 -0300

    fix: temporary tokens

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
@@ -26,7 +25,8 @@ def login(request):
     user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response({"error": "Credenciais Inválidas."}, status=status.HTTP_401_UNAUTHORIZED)
-    token, created = Token.objects.get_or_create(user=user)
+    Token.objects.filter(user=user).delete()
+    token = Token.objects.create(user=user)
     serializer = UserSerializer(instance=user)
     return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
