85ee4e42f8f1199d95b962a415276ff4e4cff9c2
VslVictor7 <vic.collfac358@outlook.com>
2025-04-12 16:48:20 -0300

    remove: authtoken

--- api/settings.py
+++ api/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'main',
     'accounts',
     'corsheaders',
