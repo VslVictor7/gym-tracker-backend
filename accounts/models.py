0d1d7de88fd37c9ffc736bb12d4005b24f1dde12
VslVictor7 <vic.collfac358@outlook.com>
2025-04-20 14:35:53 -0300

    feat: Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
 
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
     def __str__(self):
         return self.username
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Perfil'
