f917c9cee042692a510d840d275f564c9e7a883d
VslVictor7 <vic.collfac358@outlook.com>
2025-05-01 20:08:23 -0300

    chore: brazilian-portuguese project.

--- api/settings.py
+++ api/settings.py
@@ -128,9 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
+USE_L10N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
