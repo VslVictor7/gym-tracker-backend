639419c4aa53253363bef0d068b141d5884df894
VslVictor7 <vic.collfac358@outlook.com>
2025-08-31 12:59:17 -0300

    chore: making workflow possible for activity

--- accounts/tests.py
+++ accounts/tests.py
@@ -95,7 +95,7 @@ class LogoutViewTestCase(APITestCase):
     def test_logout_success(self):
         response_logout = self.client.post(self.logout_url)
 
-        self.assertEqual(response_logout.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response_logout.status_code, status.HTTP_200_OK)
         self.assertEqual(response_logout.data['message'], "Logout realizado com sucesso.")
 
 
